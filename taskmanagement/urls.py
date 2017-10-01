12ba6382e8d2c39b9a5881f731552453cf4fe1d0
Anand, Astik <Astik.Anand@cerner.com>
2017-10-01 20:30:24 +0530

    TaskManagement-1.0 Release

--- taskmanagement/urls.py
+++ taskmanagement/urls.py
@@ -6,3 +6,7 @@ urlpatterns = [
     url(r'^task/', include('task.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+
+admin.site.site_header = 'TaskManagement Admin Panel'
+admin.site.site_title = 'TaskManagement Admin Panel'
